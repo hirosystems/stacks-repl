0f5e8101435fdcc29960f43333f2baa09f9570ba
MicaiahReid <micaiahreid@gmail.com>
2023-05-23 12:01:47 -0400

    hacky way to run code snippets

--- src/components/code-editor.js
+++ src/components/code-editor.js
@@ -13,10 +13,89 @@ export class CodeEditor extends React.Component {
     console.log("onChange", newValue); // eslint-disable-line no-console
   };
 
+  fetchStacksDependencies = async () => {
+    if (this.stacksDeps) return;
+    if (!this.stacksProm) {
+      const deps = ["network"];
+      this.stacksProm = Promise.all([
+        new Promise((resolve) => {
+          require([`../../node_modules/@stacks/network`], (stacksPackage) => {
+            resolve(stacksPackage);
+          });
+        }),
+        new Promise((resolve) => {
+          require([`../../node_modules/@stacks/connect`], (stacksPackage) => {
+            resolve(stacksPackage);
+          });
+        }),
+        new Promise((resolve) => {
+          require([`../../node_modules/@stacks/auth`], (stacksPackage) => {
+            resolve(stacksPackage);
+          });
+        }),
+        new Promise((resolve) => {
+          require([`../../node_modules/@stacks/common`], (stacksPackage) => {
+            resolve(stacksPackage);
+          });
+        }),
+        new Promise((resolve) => {
+          require([`../../node_modules/@stacks/transactions`], (
+            stacksPackage
+          ) => {
+            resolve(stacksPackage);
+          });
+        }),
+        new Promise((resolve) => {
+          require([`../../node_modules/@stacks/stacking`], (stacksPackage) => {
+            resolve(stacksPackage);
+          });
+        }),
+      ]);
+      this.stacksDeps = await this.stacksProm;
+    } else {
+      this.stacksDeps = await this.stacksProm;
+    }
+  };
+
   editorDidMount = async (editor) => {
     // eslint-disable-next-line no-console
     console.log("editorDidMount", editor, editor.getValue(), editor.getModel());
     this.editor = editor;
+    this.fetchStacksDependencies();
+  runit = async () => {
+    const userCode = this.editor.getValue();
+
+    if (!this.stacksDeps) {
+      await this.fetchStacksDependencies();
+    }
+
+    const AsyncFunction = Object.getPrototypeOf(
+      async function () {}
+    ).constructor;
+
+    console.log(this.stacksDeps);
+    const fn = new AsyncFunction(
+      "stacksNetwork",
+      "stacksConnect",
+      "stacksAuth",
+      "stacksCommon",
+      "stacksTransactions",
+      "stacksStacking",
+      `with ({...stacksNetwork, ...stacksConnect, ...stacksAuth, ...stacksCommon, ...stacksTransactions, ...stacksStacking })
+      try {
+        return  (async () => {
+          "use strict";
+          {
+            ${code}
+          }
+        })();
+    } catch(e) {
+        console.log(e);
+    }`
+    );
+    fn(...this.stacksDeps);
+  };
+
   render() {
     const { code } = this.state;
     const options = {
@@ -37,6 +116,7 @@ export class CodeEditor extends React.Component {
           editorDidMount={this.editorDidMount}
           theme="vs-dark"
         />
+        <button onClick={this.runit}>Run It</button>
       </div>
     );
   }
